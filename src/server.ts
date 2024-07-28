e459394bc03fa762b118a929a0fbb8cecd301062
Pujon Das Auvi <auvipujondas@gmail.com>
2024-07-28 10:19:25 +0600

    server file added

--- src/server.ts
+++ src/server.ts
@@ -1,3 +1,21 @@
 import { Server } from "http";
+import mongoose from "mongoose";
+import config from "./app/config";
+import app from "./app";
 
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
 let server: Server;
+
+async function main() {
+  try {
+    await mongoose.connect(config.database_url as string);
+
+    server = app.listen(config.port, () => {
+      console.log(`app is running on http://localhost:${config.port}`);
+    });
+  } catch (err) {
+    console.log(err);
+  }
+}
+
+main();
