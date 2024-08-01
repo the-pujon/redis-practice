8368226b0f086a2d514f6474e29e7a2df4ed2b46
Pujon Das Auvi <auvipujondas@gmail.com>
2024-08-01 14:47:38 +0600

    globalErrorHandler function added for better error handling

---
+++ src/app/middlewares/globalErrorHandler.ts
@@ -0,0 +1,72 @@
+import { ErrorRequestHandler } from "express";
+import { TErrorSources as TErrorMessage } from "../interface/error";
+import { ZodError } from "zod";
+import handleZodError from "../errors/handleZodError";
+import config from "../config";
+import handleValidationError from "../errors/handleValidationError";
+import handleCastError from "../errors/handleCastError";
+import handleDuplicateError from "../errors/handleDuplicateError";
+import AppError from "../errors/AppError";
+
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
+const globalErrorHandler: ErrorRequestHandler = (err, req, res, next) => {
+  let statusCode = 500;
+  let message = "Something went wrong!";
+  let errorMessages: TErrorMessage = [
+    {
+      path: "",
+      message: "Something went wrong",
+    },
+  ];
+
+  if (err instanceof ZodError) {
+    const simplifiedError = handleZodError(err);
+    statusCode = simplifiedError.statusCode;
+    message = simplifiedError.message;
+    errorMessages = simplifiedError.errorSources;
+  } else if (err?.name === "ValidationError") {
+    const simplifiedError = handleValidationError(err);
+    statusCode = simplifiedError.statusCode;
+    message = simplifiedError.message;
+    errorMessages = simplifiedError.errorSources;
+  } else if (err?.name === "CastError") {
+    const simplifiedError = handleCastError(err);
+    statusCode = simplifiedError.statusCode;
+    message = simplifiedError.message;
+    errorMessages = simplifiedError.errorSources;
+  } else if (err?.code === 11000) {
+    const simplifiedError = handleDuplicateError(err);
+    statusCode = simplifiedError.statusCode;
+    message = simplifiedError.message;
+    errorMessages = simplifiedError.errorSources;
+  } else if (err instanceof AppError) {
+    statusCode = err?.statusCode;
+    message = err.message;
+    errorMessages = [
+      {
+        path: "",
+        message: err?.message,
+      },
+    ];
+  } else if (err instanceof Error) {
+    message = err.message;
+    errorMessages = [
+      {
+        path: "",
+        message: err?.message,
+      },
+    ];
+  }
+
+  return res.status(statusCode).json({
+    success: false,
+    ...(statusCode === 404 || statusCode === 401 ? { statusCode } : null),
+    message,
+    ...(statusCode !== 404 && statusCode !== 401 && { errorMessages }),
+    ...(statusCode !== 404 &&
+      statusCode !== 401 &&
+      config.NODE_ENV === "development" && { stack: err?.stack }),
+  });
+};
+
+export default globalErrorHandler;
